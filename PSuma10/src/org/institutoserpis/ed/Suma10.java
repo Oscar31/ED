f010b51ce57af54abe6d562501a9ce7de6f9f16a
oscar31 <1dam.oinvernon@institutoserpis.org>
2014-11-19 09:04:11 +0100

    Añadido PSuma10

---
+++ PSuma10/src/org/institutoserpis/ed/Suma10.java
@@ -0,0 +1,26 @@
+package org.institutoserpis.ed;
+
+public class Suma10 {
+
+	public static void main(String[] args) {
+		
+		int suma=0;
+				
+	for(int i=1;i<=10;i++){
+			
+		suma = suma+i;
+		System.out.println("la suma es "+suma );
+	
+		}
+//	int numero = 1;
+//			while (numero<=10){
+//				suma= suma + numero;
+//			numero++;		
+//			}
+
+	//	System.out.println("la suma es "+suma );
+
+		
+	}
+
+}
